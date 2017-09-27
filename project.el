3631c6bedebcc270cede4353e9e62220138d7303
Alexander Lutsai <s.lyra@ya.ru>
2017-09-27 20:44:17 +0300

    garbage

--- project.el
+++
@@ -1,6 +0,0 @@
-(ede-add-project-to-global-list
- (ede-compdb-project "osfi-z"
-                     :name "osfi-z"
-		     :file "/home/lyra/prj/osfi-z/CMakeLists.txt"
-		     :build-command "cd /home/lyra/prj/osfi-z/build; make;"
-		     :compdb-file "/home/lyra/prj/osfi-z/build/compile_commands.json"))
