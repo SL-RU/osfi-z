855b5a932f824db6a16579730870c7f4369520b8
Alexander Lutsai <s.lyra@ya.ru>
2017-12-08 03:24:08 +0300

    fix

--- osfi-z/player/warble_playlist.h
+++ osfi-z/player/warble_playlist.h
@@ -2,8 +2,30 @@
 #define WARBLE_PLAYLIST_H
 #include "warble.h"
 
+typedef enum {
+    WPlaylist_Normal,
+    WPlaylist_Audiobook,
+    WPlaylist_CUE,
+} WPlaylistType;
+
 typedef struct {
+    char *name;
     
+    WPlaylistType type;
+    void *data;
+    struct {
+	void (*oncomplete)(WTrack *track);
+	void (*state_update)(WTrack *track);
+	void (*metadata_update)(WTrack *track);
+	void (*time_elapsed)(WTrack *track, uint32_t time);
+    } handlers; //handlers of specific events
+
+    void (*getnext)();
+    void (*getprev)();
+    void (*getcount)();
+    void (*getbyindex)();
 } WPlaylist;
+
+
 
 #endif
